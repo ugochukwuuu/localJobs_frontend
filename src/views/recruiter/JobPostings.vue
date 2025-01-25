c40e3f06a5c1c757c0eca8e7df6d6fc609678b14
Promise Okeke <ugobright31@gmail.com>
2025-01-25 19:20:50 +0100

    job-action-buttons

--- src/views/recruiter/JobPostings.vue
+++ src/views/recruiter/JobPostings.vue
@@ -41,7 +41,22 @@ onMounted(() => {
     <div v-if="jobsDataLoading" class="loader d-flex align-items-center justify-content-center">
         <i class="pi pi-spin pi-spinner"></i>
     </div>
+
   <div v-if="!jobsDataLoading" class="job-postings raleway">
+    <div class="job-action-buttons">
+        <div @click="addJobFunc" class="job-action-button add-job-btn align-items-center  flex-row cursor ">
+        <i class="pi pi-plus"></i>
+        <p>Add a job</p>
+        </div>
+        <div @click="" class="job-action-button edit-job-btn  align-items-center  flex-row cursor">
+        <i class="pi pi-pencil"></i>
+        <p>Edit Job</p>
+        </div>
+        <div @click="" class="job-action-button delete-job-btn align-items-center  flex-row cursor">
+        <i class="pi pi-trash"></i>
+        <p>Delete Job</p>
+        </div>
+    </div>
     <h1 class="raleway">Job Postings</h1>
     <p>You have posted {{ postedJobs.length }} jobs.</p>
 
@@ -50,10 +65,7 @@ onMounted(() => {
         <JobCard v-for="job in postedJobs" :ket="job.id" :job = "job"/>
     </div>
 
-    <div @click="addJobFunc" class="add-job-button align-items-center  flex-column cursor">
-    <i class="pi pi-plus"></i>
-    <p>Add a job</p>
-    </div>
+ 
     
     <AddJobModal v-if="store.state.showAddJob"/>
   </div>
